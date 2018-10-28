77e93faa73ca5a283f49c062f4eab54d4446c76a
Paul Berg <hello@paulrberg.com>
2018-10-28 10:34:56 +0100

    Added Buttons and image to README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 The frontend app for the users of [Flipper](https://github.com/PaulRBerg/CryptoLife/tree/Honeybadgerz_Flipper).
+
+![Flipper UI](screens.jpeg)
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Buttons from './components/Buttons'
 
 ReactDOM.render(<App />, document.getElementById('root'));
